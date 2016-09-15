7070911a50a318ca642304daff6e0c50ba632bfb
jinchenglee <jincheng.li@gmail.com>
2016-09-14 22:53:10 -0700

    Modified DQN network to be CNN based, and storing 4 frames of screens as one observation. The average reward is showing good trend, ex: episode:  0 Evaluation Average Reward:  -3.040000000000002 episode:  1000 Evaluation Average Reward:  0.2510000000000042 episode:  2000 Evaluation Average Reward:  0.6200000000000182 episode:  3000 Evaluation Average Reward:  0.8720000000000132 episode:  4000 Evaluation Average Reward:  2.1970000000000005 episode:  5000 Evaluation Average Reward:  5.097000000000065 episode:  6000 Evaluation Average Reward:  6.131000000000143 episode:  7000 Evaluation Average Reward:  4.219000000000056 episode:  8000 Evaluation Average Reward:  6.050000000000098 episode:  9000 Evaluation Average Reward:  10.662999999999892

--- examples/snake_dqn_agent.py
+++ examples/snake_dqn_agent.py
@@ -10,13 +10,14 @@ import tensorflow as tf
 #---------------------------------------------------------------
 # Hyper Parameters for DQN
 NUM_CHANNELS = 3
-HIDDEN_LAYER_DEPTH = 1000
+HIDDEN_LAYER_DEPTH = 512
 
 GAMMA = 0.9 # discount factor for target Q
 INITIAL_EPSILON = 0.5 # starting value of epsilon
 FINAL_EPSILON = 0.01 # final value of epsilon
 REPLAY_SIZE = 10000 # experience replay buffer size
-BATCH_SIZE = 32 # size of minibatch
+BATCH_SIZE = 64 # size of minibatch
+FRAME_BUF_DEPTH = 4 # number of frames to keep in history
 
 class DQN_Agent():
     """
@@ -32,17 +33,17 @@ class DQN_Agent():
         print("valid actions list: ", self.actions, "action_dim: ", self.action_dim)
         
         # Image screen resolution 
-        self.state_dim_list = list(env.getScreenDims())
-        self.state_dim_list.extend([NUM_CHANNELS])
-        print("game screen dim: ", self.state_dim_list)
-        self.state_dim = self.state_dim_list[0]*self.state_dim_list[1]*self.state_dim_list[2]
+        self.state_dim = list(env.getScreenDims())
+        # History of frames to keep in training data
+        self.state_dim.extend([FRAME_BUF_DEPTH])
+        print("game screen dim: ", self.state_dim)
 
         # Some more parameters init
         self.time_step = 0
         self.epsilon = INITIAL_EPSILON
 
         # Create network
-        self.create_DQN()
+        self.create_DQN_conv()
         self.create_tensorflow()
 
         # Init session
@@ -50,22 +51,37 @@ class DQN_Agent():
         self.session.run(tf.initialize_all_variables())
 
         # Add ops to save and restore all the variables.
-        self.saver = tf.train.Saver()
+        #self.saver = tf.train.Saver()
 
 
-    def create_DQN(self):
+    def conv2d(self, x, W, b, strides=1):
+        # Conv2D wrapper, writh bias and relu activation
+        x = tf.nn.conv2d(x, W, strides=[1,strides,strides,1], padding='SAME')
+        x = tf.nn.bias_add(x, b)
+        return tf.nn.relu(x)
+
+    # Convolutional NN impl.
+    def create_DQN_conv(self):
         # network weights
-        W1 = self.weight_variable([self.state_dim,HIDDEN_LAYER_DEPTH])
-        b1 = self.bias_variable([HIDDEN_LAYER_DEPTH])
-        W2 = self.weight_variable([HIDDEN_LAYER_DEPTH,self.action_dim])
-        b2 = self.bias_variable([self.action_dim])
-        # input layer
-        self.state_input = tf.placeholder(tf.float32, [None, self.state_dim])
-        #print("reshaped dim: ", self.state_input)
-        # hidden layers
-        h_layer = tf.nn.relu(tf.matmul(self.state_input,W1) + b1)
+        Wc1 = self.weight_variable([3,3,4,16])
+        bc1 = self.bias_variable([16])
+        Wc2 = self.weight_variable([3,3,16,32])
+        bc2 = self.bias_variable([32])
+        Wfc = self.weight_variable([self.state_dim[0]*self.state_dim[1]*32,HIDDEN_LAYER_DEPTH])
+        bfc = self.bias_variable([HIDDEN_LAYER_DEPTH])
+        Wout = self.weight_variable([HIDDEN_LAYER_DEPTH,self.action_dim])
+        bout = self.bias_variable([self.action_dim])
+        # input layer - [batch size, screen_X, screen_Y, num_frame]
+        self.state_input = tf.placeholder(tf.float32, [None, self.state_dim[0],self.state_dim[1],self.state_dim[2]])
+        #print("state_input shape: ", self.state_input)
+        # conv layers
+        conv_layer1 = self.conv2d(self.state_input,Wc1,bc1)
+        conv_layer2 = self.conv2d(conv_layer1,Wc2,bc2)
+        # Fully connected layer
+        conv_layer2_flat = tf.reshape(conv_layer2,[-1,self.state_dim[0]*self.state_dim[1]*32])
+        fc_layer = tf.nn.relu(tf.matmul(conv_layer2_flat,Wfc) + bfc)
         # Q Value layer
-        self.Q_value = tf.matmul(h_layer,W2) + b2
+        self.Q_value = tf.matmul(fc_layer,Wout) + bout
 
     def weight_variable(self,shape):
         initial = tf.truncated_normal(shape)
@@ -83,7 +99,7 @@ class DQN_Agent():
         self.cost = tf.reduce_mean(tf.square(self.y_input - Q_value_of_action))
         self.optimizer = tf.train.AdamOptimizer(0.0001).minimize(self.cost)
 
-    def perceive(self, observation, action, reward, next_observation, Snakehead_food_distance, done):
+    def perceive(self, observation, action, reward, next_observation, done):
         one_hot_action = np.zeros(self.action_dim)
         one_hot_action[self.actions.index(action)] = 1
         #print(action, one_hot_action)
@@ -93,26 +109,30 @@ class DQN_Agent():
             self.replay_buffer.popleft()
 
         if len(self.replay_buffer) > BATCH_SIZE:
-            self.train_DQN(Snakehead_food_distance)
+            self.train_DQN()
 
-    def train_DQN(self, dist):
+    def train_DQN(self):
         self.time_step += 1
         # Step 1: obtain random minibatch from replay memory
         minibatch = random.sample(self.replay_buffer,BATCH_SIZE)
         # Create *observation_batch numpy array with right size
-        observation_batch = np.empty([1,self.state_dim])
-        next_observation_batch = np.empty([1,self.state_dim])
+        observation_batch = np.empty([1,self.state_dim[0], self.state_dim[1], FRAME_BUF_DEPTH])
+        next_observation_batch = np.empty([1,self.state_dim[0], self.state_dim[1], FRAME_BUF_DEPTH])
         action_batch = []
         reward_batch = []
         for data in minibatch:
-            observation_batch = np.concatenate((observation_batch, data[0].reshape([1,self.state_dim])), axis=0)
+            data_0 = data[0].reshape([1,self.state_dim[0], self.state_dim[1], FRAME_BUF_DEPTH])
+            data_3 = data[3].reshape([1,self.state_dim[0], self.state_dim[1], FRAME_BUF_DEPTH])
+            #print("observation_batch.shape = ", observation_batch.shape)
+            #print("data[0].shape = ", data[0].shape)
+            #print("data_0.shape = ", data_0.shape)
+            observation_batch = np.concatenate((observation_batch, data_0), axis=0)
             action_batch.append(data[1])
             reward_batch.append(data[2]) 
-            next_observation_batch = np.concatenate((next_observation_batch, data[3].reshape([1,self.state_dim])), axis=0)
+            next_observation_batch = np.concatenate((next_observation_batch, data_3), axis=0)
         # Remove the randomly created top line of *observation_batch array
         observation_batch = np.delete(observation_batch,0,axis=0)
         next_observation_batch = np.delete(next_observation_batch,0,axis=0)
-        #print("next_observation_batch.shape = ", next_observation_batch.shape)
 
         # Step 2: calculate y (Q-value of action in real play)
         y_batch = []
@@ -120,9 +140,9 @@ class DQN_Agent():
         for i in range(0,BATCH_SIZE):
             done = minibatch[i][4]
             if done:
-                y_batch.append(reward_batch[i]+dist)
+                y_batch.append(reward_batch[i])
             else:
-                y_batch.append(reward_batch[i]+dist + GAMMA * np.max(Q_value_batch[i]))
+                y_batch.append(reward_batch[i] + GAMMA * np.max(Q_value_batch[i]))
 
         self.optimizer.run(feed_dict={
                 self.y_input:y_batch,
@@ -132,15 +152,17 @@ class DQN_Agent():
 
     def saveParam(self):
         # Save the scene
-        save_path = self.saver.save(self.session, "./tmp/model_tr_"+str(self.time_step)+".ckpt")
+        #save_path = self.saver.save(self.session, "./tmp/model_tr_"+str(self.time_step)+".ckpt")
+        pass
 
     def restoreParam(self):
         # Restore the scene
-        self.saver.restore(self.session, "./tmp_d/model_tr_d.ckpt")
+        #self.saver.restore(self.session, "./tmp_e/model_tr_e.ckpt")
+        pass
 
     def pickAction(self, observation):
         Q_value = self.Q_value.eval(feed_dict = { 
-            self.state_input:[observation.reshape(self.state_dim)] 
+            self.state_input:[observation] 
             })[0]
         #print("pickAction: Estimated Q-Values = ", Q_value)
         action_index = np.argmax(Q_value)
@@ -149,7 +171,7 @@ class DQN_Agent():
 
     def pickAction_egreedy(self, observation):
         Q_value = self.Q_value.eval(feed_dict = { 
-            self.state_input:[observation.reshape(self.state_dim)] 
+            self.state_input:[observation] 
             })[0] # Evaluation take in [N, self.action_dim], thus output N of Q-values. Here N=1.
         #print("pickAction_egreedy: Estimated Q-Values = ", Q_value)
         if random.random() <= self.epsilon:
@@ -164,9 +186,18 @@ class DQN_Agent():
 #---------------------------------------------------------------
 # Hyper Parameters
 #---------------------------------------------------------------
-EPISODE = 500000 # Episode limit
+EPISODE = 50000 # Episode limit
 STEP = 100      # Step limit within one episode 
 TEST = 10       # Number of experiement test every 100 episode
+
+def luminance(RGB):
+    """
+    Convert screen RGB into luminance. [screen_X, screen_Y, channel] -> [screen_X, screen_Y]
+    """
+    #print("RGB shape:", RGB.shape)
+    lum = np.dot(RGB,[0.299,0.587,0.114])
+    #print("lum = ", lum)
+    return lum
 
 def main():
     # Init PygameLearningEnv env and agent
@@ -180,7 +211,9 @@ def main():
         env.reset_game()
         env.display_screen=False
         env.force_fps = True
-        observation = env.getScreenRGB()
+        # TODO: FIXME
+        observation = np.asarray([luminance(env.getScreenRGB())]*FRAME_BUF_DEPTH).reshape(16,16,FRAME_BUF_DEPTH)
+        #print("observation shape = ", observation.shape)
         reward = 0.0
         done = False
     
@@ -193,22 +226,29 @@ def main():
             action = agent.pickAction_egreedy(observation)
     
             reward = env.act(action)
-            next_observation = env.getScreenRGB()
+            #print("step reward = ", reward)
+
+            next_observation = np.zeros_like(observation)
+            #print("next_observation shape = ", next_observation.shape)
+            next_observation[:,:,1:] = observation[:,:,:-1]
+            next_observation[:,:,0] = luminance(env.getScreenRGB())
             done = env.game_over()
-            dist = game.getSnakeheadFoodDistance()
+            #dist = game.getSnakeheadFoodDistance()
     
-            agent.perceive(observation, action, reward, next_observation, dist, done)
+            agent.perceive(observation, action, reward, next_observation, done)
 
             observation = next_observation
     
-            #print(observation)
+            #for i in range(FRAME_BUF_DEPTH):
+            #    print("next_observation", i, " = ", next_observation[:,:,i])
+            #print("next_observation", 0, " = ", next_observation[:,:,0])
             #env.savescreen("tmp/image"+str(step_train).zfill(5)+".png")
 
             if done:
                 break
 
         # Test every 1000 espisodes
-        if episode % 10000 == 0:
+        if episode % 1000 == 0:
             total_reward = 0
             env.display_screen=True # <<JC>> View the test result?
             env.force_fps = False
@@ -220,7 +260,8 @@ def main():
                 # Initialize task
                 env.init()
                 env.reset_game()
-                observation = env.getScreenRGB()
+                # TODO: FIXME
+                observation = np.asarray([luminance(env.getScreenRGB())]*FRAME_BUF_DEPTH).reshape(16,16,FRAME_BUF_DEPTH)
                 reward = 0.0
                 done = False
             
@@ -230,7 +271,8 @@ def main():
                     action = agent.pickAction(observation)
             
                     reward = env.act(action)
-                    observation = env.getScreenRGB()
+                    observation[:,:,1:] = observation[:,:,:-1]
+                    observation[:,:,0] = luminance(env.getScreenRGB())
                     done = env.game_over()
             
                     total_reward += reward
